e79d1b5b08a717e30514d45b44ed59531596f6ea
Carlos <carlosfernandez@paucasesnovescifp.cat>
2020-10-15 18:41:28 +0200

    We made an upgrade to the exercice 3.
    Right know we can choose between 2 options: Sum and Substract.
    
    We can see the option what we choosed in a little box at bottom side of the Windows Form.

---
+++ Exercices_WinformsApp4/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Exercices_WinformsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SumButton = new System.Windows.Forms.Button();
+            this.SubstractButton = new System.Windows.Forms.Button();
+            this.OperationChoosed = new System.Windows.Forms.TextBox();
+            this.resultBox = new System.Windows.Forms.TextBox();
+            this.CalculateButton = new System.Windows.Forms.Button();
+            this.ResetButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(66, 112);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(66, 175);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // SumButton
+            // 
+            this.SumButton.Location = new System.Drawing.Point(336, 79);
+            this.SumButton.Name = "SumButton";
+            this.SumButton.Size = new System.Drawing.Size(101, 23);
+            this.SumButton.TabIndex = 2;
+            this.SumButton.Text = "Sum";
+            this.SumButton.UseVisualStyleBackColor = true;
+            this.SumButton.Click += new System.EventHandler(this.SumButton_Click);
+            // 
+            // SubstractButton
+            // 
+            this.SubstractButton.Location = new System.Drawing.Point(336, 175);
+            this.SubstractButton.Name = "SubstractButton";
+            this.SubstractButton.Size = new System.Drawing.Size(101, 23);
+            this.SubstractButton.TabIndex = 3;
+            this.SubstractButton.Text = "Substract";
+            this.SubstractButton.UseVisualStyleBackColor = true;
+            this.SubstractButton.Click += new System.EventHandler(this.SubstractButton_Click);
+            // 
+            // OperationChoosed
+            // 
+            this.OperationChoosed.Location = new System.Drawing.Point(359, 310);
+            this.OperationChoosed.Name = "OperationChoosed";
+            this.OperationChoosed.Size = new System.Drawing.Size(50, 22);
+            this.OperationChoosed.TabIndex = 4;
+            this.OperationChoosed.TextChanged += new System.EventHandler(this.OperationChoosed_TextChanged);
+            // 
+            // resultBox
+            // 
+            this.resultBox.Location = new System.Drawing.Point(574, 136);
+            this.resultBox.Name = "resultBox";
+            this.resultBox.Size = new System.Drawing.Size(100, 22);
+            this.resultBox.TabIndex = 5;
+            this.resultBox.TextChanged += new System.EventHandler(this.resultBox_TextChanged);
+            // 
+            // CalculateButton
+            // 
+            this.CalculateButton.Location = new System.Drawing.Point(119, 297);
+            this.CalculateButton.Name = "CalculateButton";
+            this.CalculateButton.Size = new System.Drawing.Size(75, 23);
+            this.CalculateButton.TabIndex = 6;
+            this.CalculateButton.Text = "Calculate";
+            this.CalculateButton.UseVisualStyleBackColor = true;
+            this.CalculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // ResetButton
+            // 
+            this.ResetButton.Location = new System.Drawing.Point(555, 296);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(75, 23);
+            this.ResetButton.TabIndex = 7;
+            this.ResetButton.Text = "Reset";
+            this.ResetButton.UseVisualStyleBackColor = true;
+            this.ResetButton.Click += new System.EventHandler(this.ResetButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(320, 290);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Operation Choosed";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(320, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Choose an operand";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ResetButton);
+            this.Controls.Add(this.CalculateButton);
+            this.Controls.Add(this.resultBox);
+            this.Controls.Add(this.OperationChoosed);
+            this.Controls.Add(this.SubstractButton);
+            this.Controls.Add(this.SumButton);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button SumButton;
+        private System.Windows.Forms.Button SubstractButton;
+        private System.Windows.Forms.TextBox OperationChoosed;
+        private System.Windows.Forms.TextBox resultBox;
+        private System.Windows.Forms.Button CalculateButton;
+        private System.Windows.Forms.Button ResetButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Exercices_WinformsApp4/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercices_WinformsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OperationChoosed_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SumButton_Click(object sender, EventArgs e)
+        {
+            OperationChoosed.Text = "+";
+        }
+
+        private void SubstractButton_Click(object sender, EventArgs e)
+        {
+            OperationChoosed.Text = "-";
+        }
+
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            if (OperationChoosed.Text == "+")
+            {
+                int Operand1 = Int32.Parse(textBox1.Text);
+                int Operand2 = Int32.Parse(textBox2.Text);
+                int Result = Operand1 + Operand2;
+
+                resultBox.Text = Result.ToString();
+            }
+            else if (OperationChoosed.Text == "-")
+            {
+                int Operand1 = Int32.Parse(textBox1.Text);
+                int Operand2 = Int32.Parse(textBox2.Text);
+                int Result = Operand1 - Operand2;
+
+                resultBox.Text = Result.ToString();
+            }
+        }
+        private void resultBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+            textBox2.Text = "0";
+            resultBox.Text = "0";
+        }
+    }
+}
+
