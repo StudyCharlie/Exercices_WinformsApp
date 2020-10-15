972805f3b0d0b90393f5347ddead5172031948b5
Carlos <carlosfernandez@paucasesnovescifp.cat>
2020-10-15 20:54:18 +0200

    We have done a Windows Form application with 2 classroms.
    In the first one we put 4 students, in the second one is the same.
    If you click on the first classroom it shows the members of the first classroom and work equals with the second classroom.
    When you click on a student it shows on the textBox, and finally if you click on Clear list it deletes the classroom.

---
+++ Exercice_WinformsApp8/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercice_WinformsApp8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Classroom1Button_Click(object sender, EventArgs e)
+        {
+            listBox.Items.Clear();
+            listBox.Items.Add("Carlos");
+            listBox.Items.Add("Toni");
+            listBox.Items.Add("Xesc");
+            listBox.Items.Add("Emilio");
+            
+        }
+
+        private void Classroom2Button_Click(object sender, EventArgs e)
+        {
+            listBox.Items.Clear();
+            listBox.Items.Add("Miquel");
+            listBox.Items.Add("Jose");
+            listBox.Items.Add("Juan");
+            listBox.Items.Add("Luisa");
+        }
+
+        private void listBox_SelectedValueChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = listBox.SelectedItem.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clearListButton_Click(object sender, EventArgs e)
+        {
+            listBox.Items.Clear();
+        }
+    }
+}
