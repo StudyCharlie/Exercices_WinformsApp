75cf1a48df221169105577223a1fa42cf80c4b4d
Carlos <carlosfernandez@paucasesnovescifp.cat>
2020-10-15 20:23:45 +0200

    I made a comboBox with 5 options (colors).
    The program just works easily if we have do an if what shows a message in every selected Index.

---
+++ Exercices_WinformsApp7/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Exercices_WinformsApp7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorsComboBox = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // colorsComboBox
+            // 
+            this.colorsComboBox.FormattingEnabled = true;
+            this.colorsComboBox.Items.AddRange(new object[] {
+            "Yellow",
+            "Red",
+            "Blue",
+            "White",
+            "Black"});
+            this.colorsComboBox.Location = new System.Drawing.Point(222, 62);
+            this.colorsComboBox.Name = "colorsComboBox";
+            this.colorsComboBox.Size = new System.Drawing.Size(116, 24);
+            this.colorsComboBox.TabIndex = 0;
+            this.colorsComboBox.SelectedIndexChanged += new System.EventHandler(this.colorsComboBox_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(56, 371);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(616, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.colorsComboBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox colorsComboBox;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Exercices_WinformsApp7/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercices_WinformsApp7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void colorsComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(colorsComboBox.SelectedIndex == 0)
+            {
+                textBox1.Text = "The choosen color is: Yellow";
+
+            }else if (colorsComboBox.SelectedIndex == 1)
+            {
+                textBox1.Text = "The choosen color is: Red";
+                
+            }
+            else if (colorsComboBox.SelectedIndex == 2)
+            {
+                textBox1.Text = "The choosen color is: Blue";
+            }
+            else if (colorsComboBox.SelectedIndex == 3)
+            {
+                textBox1.Text = "The choosen color is: White";
+            }
+            else if (colorsComboBox.SelectedIndex == 4)
+            {
+                textBox1.Text = "The choosen color is: Black";
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
